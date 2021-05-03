ac44385863eeec440828bd467b7b82492d4a74c2
juancsr <jcsarmientor@correo.udistrital.edu.co>
2021-05-02 22:16:08 -0500

    Territorio titulo

--- reto_1/page/src/App.js
+++ reto_1/page/src/App.js
@@ -5,7 +5,7 @@ import Graph from './components/graph/graph';
 function App() {
   return (
     <div>
-      <h1>Vacunas Covid-19 2021</h1>
+      <h1 style={{textAlign: 'center', margin: 'auto', fontSize: 20}}>Vacunas Covid-19 2021</h1>
       <div>
         <div id="menu-options">
           <XAsisSelect />
