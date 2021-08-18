4fca368588af3208baeb32a9263043f1b47db8d3
juancsr <juancsr@protonmail.com>
2021-08-18 01:32:57 -0500

    matplotlib added for visualization

--- pandas:visualizar/visualizar.py
+++ pandas:visualizar/visualizar.py
@@ -7,10 +7,16 @@ colormaps (aplicar a cualquier tipo de gráfica).
 import os
 import numpy as np
 import pandas as pd
-import requests as req
-from io import StringIO
+import matplotlib.pyplot as plt
 
 csv_path = os.path.abspath('pandas:visualizar/developer_survey_2020/survey_results_public.csv')
 df = pd.read_csv(csv_path)
 print(df.index, df.columns)
 print(df)
+
+df = df.cumsum()
+
+plt.figure()
+plt.show()
+
+#plt.close("all")
